af67bdd8daa1c546ab6f41099afbc6d96a2bb4cf
hqrd <ju.pruvost@gmail.com>
2018-10-27 19:33:16 +0200

    First commit, empty project
    - added basic backstack management between fragments

---
+++ README.md
@@ -0,0 +1,2 @@
+# GestionLille1
+
---
+++ app/src/main/java/com/tac/hqrd/gestionlille1/ui/main/AddFragment.kt
@@ -0,0 +1,49 @@
+package com.tac.hqrd.gestionlille1.ui.main
+
+import android.arch.lifecycle.ViewModelProviders
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.tac.hqrd.gestionlille1.MainActivity.Companion.addFragmentFromFragment
+import com.tac.hqrd.gestionlille1.R
+import kotlinx.android.synthetic.main.add_fragment.*
+import java.util.logging.Logger
+
+class AddFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = AddFragment()
+    }
+
+    private lateinit var viewModel: MainViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.add_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+        // TODO: Use the ViewModel
+
+        button.setOnClickListener { _ ->
+            fragmentManager?.let { fragmentManager ->
+                addFragmentFromFragment(
+                    fragmentManager,
+                    ListFragment.newInstance(),
+                    this
+                )
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Logger.getLogger(javaClass.toString()).info("RESUME")
+    }
+}
